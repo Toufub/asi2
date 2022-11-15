49994e729f0257850114f2078a0813d85e6a1baa
Eloi <eloi.cambray-lagassy@outlook.fr>
2022-11-15 16:35:13 +0100

    ajax function

--- Atelier2/chat/www/js/scripts.js
+++ Atelier2/chat/www/js/scripts.js
@@ -4,6 +4,8 @@ var messages = document.getElementById('messages');
 var form = document.getElementById('form');
 var input = document.getElementById('input');
 
+var users = [];
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     if (input.value) {
@@ -18,3 +20,14 @@ socket.on('chat message', function(msg) {
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
+
+function get_users_list() {
+    send_ajax_request("http://localhost:8081/users")
+}
+
+function send_ajax_request(url, method, callback) {
+    const Http = new XMLHttpRequest();
+    Http.open(method, url);
+    Http.send();
+    Http.onreadystatechange = callback;
+}
