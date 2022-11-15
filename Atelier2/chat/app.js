86935423e937bc5a6f330a96c89f6e5228f74807
Eloi <eloi.cambray-lagassy@outlook.fr>
2022-11-15 15:42:53 +0100

    chat basique

--- Atelier2/chat/app.js
+++ Atelier2/chat/app.js
@@ -1,28 +1,24 @@
-const bodyParser = require('body-parser')
-
-const express = require("express");
+const express = require('express');
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const port = 3000;
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static('public'));
-
-app.get('/', function (req, res) {   res.sendFile('index.html', { root: __dirname })});
-
-app.get('/json', function (req, res) {   res.status(200).json({"message":"ok"})});// établissement de la connexion
-//Whenever someone connects this gets executed
-io.on('connection', function(socket) {
-    console.log('A user connected');
-
-    //Whenever someone disconnects this piece of code executed
-    socket.on('disconnect', function () {
-        console.log('A user disconnected');
-    });
-    socket.on("message", function (message) {
-        console.log(message);
-    })
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
-server.listen(port, function () { console.log('Votre app est disponible sur localhost:3000 !')});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.broadcast.emit('hi');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
