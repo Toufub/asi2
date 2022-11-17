d7b05a23fad6015c31def2f1d1e8a350ee5b1361
Baptiste PELLARIN <baptiste@swano-lab.net>
2022-11-17 14:03:02 +0100

    netoyage v1

---
+++ atelier_1/appli/micro-auth/src/main/java/com/asi2/auth/MicroAuth.java
@@ -0,0 +1,13 @@
+package com.asi2.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAuth {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroAuth.class, args);
+	}
+
+}
